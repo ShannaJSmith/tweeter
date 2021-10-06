a472dc2c27ace1905e29367ed6ea1277b36fc7bb
ShannaJSmith <shanna_smith987@hotmail.com>
2021-10-06 19:43:23 +0000

    implemented dynamic tweets in client.js with createTweetElement and renderTweets functions. And emptied index.html container with hardcoded tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,37 +4,76 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// $(document).ready(function() {
-// });
-//jQuery shortform of document.ready:
+// Sample data from initial-tweets.json
+const data = [
+  {
+    user: {
+      name: 'Newton',
+      avatars: 'https://i.imgur.com/73hZDYK.png',
+      handle: '@SirIsaac',
+    },
+    content: {
+      text: 'If I have seen further it is by standing on the shoulders of giants',
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: 'Descartes',
+      avatars: 'https://i.imgur.com/nlhLi3I.png',
+      handle: '@rd',
+    },
+    content: {
+      text: 'Je pense , donc je suis',
+    },
+    created_at: 1461113959088,
+  },
+];
 
+// $(document).ready(function() {
+// });   <-jQuery shortform of document.ready:
+
+// returns a tweet <article> element containing the full HTML of the tweet
 $(() => {
   const renderTweets = (tweets) => {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
+    // loops through tweets
+    for (const tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet)
+      // takes return value and appends it to the tweets container. "prepend" makes the latest posts come first
+      $('#tweets-container').prepend($tweet);
+    }
   }
+  //Creating the tweet element
   const createTweetElement = (tweet) => {
-    const $tweet = $(`<article class="tweet">Hello world</article>`);
+    const $tweet = `<article class="article-tweets">
+    <header class="article-tweet-header">
+      <div class="user-profile">
+        <img class="user-icon" src=${tweet.user.avatars}/>
+      <h5>${tweet.user.name}</h5>
+    </div>
+    <div class="username">
+      <h5>${tweet.user.handle}</h5>
+    </div>
+  </header>
+      <p class="tweet-text">${tweet.content.text}</p>
+    <footer>
+      <p class="date">${timeago.format(tweet.created_at)}</p>
+      <section class="icons">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+      </section>
+    </footer>
+  </article>`
     return $tweet;
   }
   renderTweets(data);
-  // const $tweet = createTweetElement(tweetData);
-  // $('#tweets-container').append($tweet);
-  // const timePassed = timeago.format(tweetData.created_at);
-  // console.log('TWEET HERE:', $tweet);
+  const $form = $('#tweet-form');
+  $form.on('submit', function(event) {
+    //stops page from reloading (the default setting of form)
+    event.preventDefault();
+    //const serializedData = $(this).serialize();
+  })
 })
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
- // to add it to the page so we can make sure it's got all the right elements, classes, etc.
