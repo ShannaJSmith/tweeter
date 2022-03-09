0b5b991b3c92133fb953ed97cf331a1e09207754
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-03-08 21:38:10 -0500

    change some styling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $('#tweet-text').on('input', function() {
+$(document).ready(function () {
+  $('#tweet-text').on('input', function () {
     const count = $(this).val().length;
     let counter = 140 - count;
     $('.tweet-button-counter').children('.counter').text(counter);
@@ -10,3 +10,10 @@ $(document).ready(function() {
     }
   });
 });
+
+// alternative style remaining characters
+// if(remainingChar < 0) {
+//   $counter.css('color', 'red');
+// } else {
+//   $counter.css('color', 'black');
+// }
